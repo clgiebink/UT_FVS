d508ee3da2e41759803492a4843c757c3aea8f4f
Courtney Giebink <clgiebink@arizona.email.edu>
2019-07-19 15:38:25 -0700

    Add more species to calculation. Update ccf calculation to incorporate SPCD 475.

--- scripts/CCF.R
+++ scripts/CCF.R
@@ -4,7 +4,7 @@
 #4-24-19
 
 #load data
-load(file = "./data/formatted/glmm.data.imputed")
+load(file = "./data/formatted/density_data")
 
 #Crown Competition Factor
 #measure of stand density
@@ -15,44 +15,55 @@ load(file = "./data/formatted/glmm.data.imputed")
 
 
 #dataframe of species specific coefficients for ccf calculation
-ccf_df <- data.frame(species=c(93,202,122,15,19,65,96,106,108,133,321),
-                     #93=ES,202=DF,122=PP,15=WF,19=AF,65=UJ,96=BS,106=PI,108=LP,133=PM,321=OH
-                     r1 = c(0.03,0.11,0.03,0.04,0.03,0.01925,0.03,0.01925,0.01925,0.01925,0.03),
-                     r2 = c(0.0173,0.0333,0.0180,0.0270,0.0216,0.01676,0.0173,0.01676,0.01676,0.01676,0.0215),
-                     r3 = c(0.00259,0.00259,0.00281,0.00405,0.00405,0.00365,0.00259,0.00365,0.00365,0.00365,0.00363),
-                     r4 = c(0.007875,0.017299,0.007813,0.015248,0.011402,0.009187,0.007875,0.009187,0.009187,0.009187,0.011109),
-                     r5 = c(1.7360,1.5571,1.7680,1.7333,1.7560,1.76,1.736,1.76,1.76,1.76,1.7250)) #can add more species later 
+ccf_df <- data.frame(species=c(93,202,122,15,19,65,96,106,108,133,321,66,475,113,746,814,102),
+                     #93=ES,202=DF,122=PP,15=WF,19=AF,65=UJ,66=RM,96=BS,106=PI,108=LP,113=LM,133=PM,321=OH,475=MC,746=AS,814=GO,102=bristlecone pine
+                     r1 = c(0.03,0.11,0.03,0.04,0.03,0.01925,0.03,0.01925,0.01925,0.01925,0.03,0.01925,0.0204,0.01925,0.03,0.03,0.01925),
+                     r2 = c(0.0173,0.0333,0.0180,0.0270,0.0216,0.01676,0.0173,0.01676,0.01676,0.01676,0.0215,0.01676,0.0246,0.01676,0.0238,0.0215,0.01676),
+                     r3 = c(0.00259,0.00259,0.00281,0.00405,0.00405,0.00365,0.00259,0.00365,0.00365,0.00365,0.00363,0.00365,0.0074,0.00365,0.00490,0.00363,0.00365),
+                     r4 = c(0.007875,0.017299,0.007813,0.015248,0.011402,0.009187,0.007875,0.009187,0.009187,0.009187,0.011109,0.009187,0,0.009187,0.008915,0.011109,0.009187),
+                     r5 = c(1.7360,1.5571,1.7680,1.7333,1.7560,1.76,1.736,1.76,1.76,1.76,1.7250,1.76,0,1.76,1.78,1.725,1.76)
+                     dbrk = c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,10)) #can add more species later 
 
 #empty vector to hold calculated crown competition factor
-CCF_t <- vector(mode="numeric", length=nrow(glmm.data.imputed))
-for(i in 1:nrow(glmm.data.imputed)){
-  Species <- glmm.data.imputed$SPCD[i]
+CCF_t <- vector(mode="numeric", length=nrow(density_data))
+for(i in 1:nrow(density_data)){
+  Species <- density_data$SPCD[i]
   r1 <- ccf_df$r1[ccf_df$species == Species]
   r2 <- ccf_df$r2[ccf_df$species == Species]
   r3 <- ccf_df$r3[ccf_df$species == Species]
   r4 <- ccf_df$r4[ccf_df$species == Species]
   r5 <- ccf_df$r5[ccf_df$species == Species]
-  ifelse(is.na(glmm.data.imputed$DIA_C[i]), 
-         CCF_t[i] <- NA,
-         ifelse(glmm.data.imputed$DIA_C[i] <= 0.1, 
-                CCF_t[i] <- 0.0001,
-                ifelse(glmm.data.imputed$DIA_C[i] < 1.0, 
-                       CCF_t[i] <- r4 * (glmm.data.imputed$DIA_C[i]^r5),
-                       CCF_t[i] <- r1 + (r2 * glmm.data.imputed$DIA_C[i]) + (r3 * glmm.data.imputed$DIA_C[i]^2))))
-  glmm.data.imputed$CCF_t[i] <- CCF_t[i]
+  dbrk <- ccf_df$dbrk[ccf_df$species == Species]
+  if(Species == 475){
+    ifelse(density_data$DIA_C[i] < dbrk,
+           CCF_t[i] <- density_data$DIA_C[i]*(r1+r2+r3),
+           CCF_t[i] <- r1 + (r2 * density_data$DIA_C[i]) + (r3 * density_data$DIA_C[i]^2))
+  }
+  if(Species != 475){
+    ifelse(is.na(density_data$DIA_C[i]), 
+           CCF_t[i] <- NA,
+           ifelse(density_data$DIA_C[i] <= 0.1, 
+                  CCF_t[i] <- 0.0001,
+                  ifelse(density_data$DIA_C[i] < dbrk, 
+                         CCF_t[i] <- r4 * (density_data$DIA_C[i]^r5),
+                         CCF_t[i] <- r1 + (r2 * density_data$DIA_C[i]) + (r3 * density_data$DIA_C[i]^2))))
+  }
+  density_data$CCF_t[i] <- CCF_t[i]
 }
 
 #PCCF is the crown competition factor on the inventory point where the tree is established
 #dplyr
 
-glmm.data.imputed <- glmm.data.imputed %>%
+density_data <- density_data %>%
   mutate(PCCF = CCF_t * TPA_UNADJ)
 
+length(unique(density_data$PLT_CN)) #238
+
 #stand CCF = sum(CCFt on a plot)
-glmm.data.imputed <- glmm.data.imputed %>%
-  group_by(PLT_CN.y,Year) %>%
-  mutate(CCF = sum(PCCF,na.rm = TRUE)) %>%
-  mutate(CCF = ifelse(CCF == 0, NA, CCF))
+density_data <- density_data %>%
+  group_by(PLT_CN,Year) %>%
+  mutate(CCF = sum(PCCF,na.rm = TRUE)) #%>%
+  #mutate(CCF = ifelse(CCF == 0, NA, CCF))
 
 
-save(glmm.data.imputed,file = "./data/formatted/glmm.data.imputed")
+save(density_data,file = "./data/formatted/density_data")
