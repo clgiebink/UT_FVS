d14f8d53369ead8613065a0beb31c46cace811ff
Courtney Giebink <clgiebink@arizona.email.edu>
2019-09-20 14:29:35 -0700

    Fix basal area per acre calculation. Add check to see if BAL and BALIVE match.

--- scripts/BAL.R
+++ scripts/BAL.R
@@ -8,12 +8,12 @@
 load(file = "./data/formatted/density_data")
 
 #basal area
-# = dbh^2 * 0.0005454 ; converts dbh in inches to squared feet
+# = dbh^2 * 0.005454 ; converts dbh in inches to squared feet
 #basal area per acre
 #BA*tpa
 
 density_data <- density_data %>%
-  mutate(BA_pa = (DIA_C^2 * 0.0005454) * TPA_UNADJ)
+  mutate(BA_pa = ((DIA_C^2) * 0.005454) * TPA_C)
 
 
 #rank trees per year per plot
@@ -29,5 +29,10 @@ density_data <- density_data %>%
 #check to make sure dense rank is okay
 max(density_data$rank_pltyr)
 #[1] 66
+bal_check <- density_data %>%
+  group_by(PLT_CN)%>%
+  summarise(max = max(BAL))
+bal_check$BALIVE <- per_cov$BALIVE[match(bal_check$PLT_CN, per_cov$PLT_CN)]
+#between 0 and 10 off
 
 save(density_data,file = "./data/formatted/density_data")
