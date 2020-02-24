292826cfa20a6ab83829714390a42a5b888230d5
Courtney Giebink <clgiebink@email.arizona.edu>
2020-02-24 11:01:00 -0700

    Plot all trees with tree rings. Plot validation trees.

--- scripts/spatial.R
+++ scripts/spatial.R
@@ -1,15 +1,20 @@
 #map trees
 
+#including newly validated trees
+load('./data/formatted/data_all2.Rdata')
+
 library(maps)
 m = map_data('state', region = 'Utah')
 
-spatial_df <- per_cov %>%
-  filter(TRE_CN %in% data_all$TRE_CN) %>%
+spatial_df <- data_all %>%
+  ungroup() %>%
+  select(TRE_CN,LAT,LON,SPCD) %>%
+  distinct() %>%
   filter(SPCD %in% c(93,122,202)) %>%
-  select(LON,LAT,SPCD) %>%
   mutate(Species = ifelse(SPCD==93,"Engelmann spruce",
                            ifelse(SPCD==122,"Ponderosa pine",
                                   "Douglas fir")))
+length(unique(spatial_df$TRE_CN)) #318
 
 ggplot() + 
   geom_polygon( data=m, aes(x=long, y=lat,group=group),colour="black", fill="white" )+
@@ -20,3 +25,23 @@ ggplot() +
   ylab('Latitude')+
   coord_fixed() +
   theme(legend.position = "bottom")
+
+val_spatial_df <- val_dset %>%
+  ungroup() %>%
+  select(TRE_CN,LAT,LON,SPCD) %>%
+  distinct() %>%
+  filter(SPCD %in% c(93,122,202)) %>%
+  mutate(Species = ifelse(SPCD==93,"Engelmann spruce",
+                          ifelse(SPCD==122,"Ponderosa pine",
+                                 "Douglas fir")))
+
+ggplot() + 
+  geom_polygon(data=m, aes(x=long, y=lat,group=group),colour="black", fill="white" )+
+  geom_point(data=val_spatial_df,aes(x=LON,y=LAT,
+                                 color=factor(Species),shape=factor(Species),alpha = .9))+
+  ggtitle("Validation Trees in Utah")+
+  xlab('Longitude')+
+  ylab('Latitude')+
+  coord_fixed() +
+  theme(legend.position = "bottom")
+
