8975dd0abce872ecf49e9ec5b40286d2e9610de8
Courtney Giebink <clgiebink@email.arizona.edu>
2020-11-17 16:24:28 -0700

    Add calculations, which keep trees per acre constant.

--- scripts/BAL.R
+++ scripts/BAL.R
@@ -36,3 +36,27 @@ bal_check$BALIVE <- per_cov$BALIVE[match(bal_check$PLT_CN, per_cov$PLT_CN)]
 #between 0 and 10 off
 
 save(density_data,file = "./data/formatted/density_data.Rdata")
+
+# TPA constant ----
+
+load("./data/formatted/dens_data_con.Rdata")
+
+#basal area
+# = dbh^2 * 0.005454 ; converts dbh in inches to squared feet
+#basal area per acre
+#BA*tpa
+
+dens_data_con <- dens_data_con %>%
+  mutate(BA_pa = ((DIA_C^2) * 0.005454) * TPA_UNADJ)
+
+#BAL
+#sum BApa of trees larger on the same plot in same year
+
+density_data <- density_data %>%
+  group_by(PLT_CN,Year) %>%
+  #mutate(rank_pltyr = rank(DIA_C, na.last = TRUE, ties.method = "min")) %>%
+  #min assigns lowest value to ties (1,2,3,3,5,6)
+  mutate(BAL = map_dbl(DIA_C,~sum(BA_pa[DIA_C>.x],na.rm = TRUE)))
+
+save(dens_data_con,file = "./data/formatted/dens_data_con.Rdata")
+
